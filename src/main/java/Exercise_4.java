d18915315aa4c3072d754b64ba2e0ba1756e1164
spenserTac <spenserdt@gmail.com>
2021-08-24 21:29:39 -0400

    Changed println -> print.

--- src/main/java/Exercise_4.java
+++ src/main/java/Exercise_4.java
@@ -4,16 +4,16 @@ public class Exercise_4{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter a noun: ");
+        System.out.print("Enter a noun: ");
         String noun = scanner.nextLine();
 
-        System.out.println("Enter a verb: ");
+        System.out.print("Enter a verb: ");
         String verb = scanner.nextLine();
 
-        System.out.println("Enter a adjective: ");
+        System.out.print("Enter a adjective: ");
         String adj = scanner.nextLine();
 
-        System.out.println("Enter a adverb: ");
+        System.out.print("Enter a adverb: ");
         String adverb = scanner.nextLine();
 
         System.out.println("Do you " + verb + " your " + adj + " " +  noun + " " + adverb +
